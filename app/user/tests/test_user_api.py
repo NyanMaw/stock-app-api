969f550937a683781051713f8af28465d7922f3f
NyanMaw <nyanmawhtun@gmail.com>
2023-02-25 06:29:50 +0800

    Checking for user API errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,7 +44,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        CREATE_user(**payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
